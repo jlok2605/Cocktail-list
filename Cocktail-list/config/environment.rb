8db2d098a2a7d117a97d09715a959008f42c15a6
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-10-23 15:06:38 +1000

    environment setup

---
+++ Cocktail-list/config/environment.rb
@@ -0,0 +1,9 @@
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3"
+    :database => "./config/cocktail.db"
+)
+
+require_relative "../lib/user"
+require_relative "../lib/saved_cocktails"
