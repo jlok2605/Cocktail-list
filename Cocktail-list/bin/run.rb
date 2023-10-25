7d6083d0f78660c4b5b74a98ee9b40e208d1c9d4
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-10-25 10:52:22 +1000

    schema made for cocktails

--- Cocktail-list/bin/run.rb
+++ Cocktail-list/bin/run.rb
@@ -5,11 +5,11 @@ class Run
         puts "Hello and welcome to your peronal cocktail list"
    
         def menu
-            puts "MENU:"
+            puts "MENU"
+            puts "Please choose from the below options:"
             puts ".random - to find a random cocktail"
             puts ".list - to view your saved list"
             puts ".exit - to exit the program"
-            puts "Please choose from the above options"
         end
     end
 
