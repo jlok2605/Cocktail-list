0a77d0a34830bca584306b04be0bf0da50997109
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-10-25 11:48:45 +1000

    exit feature working

--- Cocktail-list/bin/run.rb
+++ Cocktail-list/bin/run.rb
@@ -3,15 +3,28 @@ require_relative "../config/environment"
 class Run
     def initialize
         puts "Hello and welcome to your peronal cocktail list"
-   
-        def menu
-            puts "MENU"
-            puts "Please choose from the below options:"
-            puts ".random - to find a random cocktail"
-            puts ".list - to view your saved list"
-            puts ".exit - to exit the program"
-        end
     end
+    
+    def menu
+        puts "MENU"
+        puts "Please choose from the below options:"
+        puts ".random - to find a random cocktail"
+        puts ".list - to view your saved list"
+        puts ".exit - to exit the program"
+    end
+    def run
+        is_running = true
+        while is_running do 
+            menu
+            choice = gets.chomp.downcase
+
+            if choice.downcase == ".exit"
+                is_running = false
+                puts "Now exiting the program"
+            end
+    end
+end
+    
 
    
 end
@@ -19,4 +32,6 @@ end
  
 
 
-Run.new.menu
+run_program = Run.new
+
+run_program.run
