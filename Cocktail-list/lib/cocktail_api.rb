2923db5041daef97858877aedd3d07afc0ad4630
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-10-28 14:01:45 +1000

    testing of uri-open

--- Cocktail-list/lib/cocktail_api.rb
+++ Cocktail-list/lib/cocktail_api.rb
@@ -3,11 +3,16 @@ require 'open-uri'
 require 'json'
 
 
-class cocktailAPI
-    def self.get_random_cocktail
-        url = "www.thecocktaildb.com/api/json/v1/1/random.php"
-        uri = URI.parse(url)
-        uri.open
-    end
-end
+url = "https://www.thecocktaildb.com/api.php"
+uri = URI.parse(url)
+
+response = URI.open(url).read
+
+puts response
+# class cocktailAPI
+#     def self.get_random_cocktail
+#         url = "www.thecocktaildb.com/api/json/v1/1/random.php"
+#         uri = URI.parse(url)
+#     end
+# end
 
