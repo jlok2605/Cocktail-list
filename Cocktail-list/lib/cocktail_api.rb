087bc4042253458ae0342205f69528116b1f3edc
Jeremy Lok <lambourghinis_pone@hotmail.com>
2023-11-01 12:45:09 +1000

    cocktails display as json

--- Cocktail-list/lib/cocktail_api.rb
+++ Cocktail-list/lib/cocktail_api.rb
@@ -3,12 +3,25 @@ require 'open-uri'
 require 'json'
 
 
-url = "https://www.thecocktaildb.com/api.php"
-uri = URI.parse(url)
+url = URI("https://www.thecocktaildb.com")
+path = "/api/json/v1/1/random.php"
 
-response = URI.open(url).read
 
-puts response
+
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true if url.scheme == 'https'
+
+request = Net::HTTP::Get.new(path)
+
+response = http.request(request)
+
+if response.code =='200'
+    data = JSON.parse(response.body)
+    puts JSON.pretty_generate(data)
+else
+    puts "Error"
+end
+
 # class cocktailAPI
 #     def self.get_random_cocktail
 #         url = "www.thecocktaildb.com/api/json/v1/1/random.php"
